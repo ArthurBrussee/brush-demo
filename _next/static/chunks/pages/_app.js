6e7139b761f2cb009cc70c0f34c43806b5975abd
Arthur Brussee <arthur.brussee@gmail.com>
2025-07-21 15:55:17 +0100

    Update to Next.JS version

---
+++ _next/static/chunks/pages/_app.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c647894a3efc3803.js",
+  "static/chunks/2033735c7b10f1b9.js",
+  "static/chunks/40de67a0654b14fb.js"
+])
---
+++ _next/static/chunks/pages/_error.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/f02c460f9b7a96aa.js",
+  "static/chunks/2033735c7b10f1b9.js",
+  "static/chunks/a7849171bb5f40d1.js"
+])
