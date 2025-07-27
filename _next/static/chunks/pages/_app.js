197a9234532c68b1f43bc212c5d9a59cc51e41af
Arthur Brussee <arthur.brussee@gmail.com>
2025-07-27 15:48:45 +0100

    Try basePath again

--- _next/static/chunks/pages/_app.js
+++ _next/static/chunks/pages/_app.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/36e7f54fe2e9aa85.js",
-  "static/chunks/7f4920b4ef105f50.js",
-  "static/chunks/2211587feca5d9c3.js"
+  "static/chunks/a05c703d8e221053.js",
+  "static/chunks/c32f169d91dc2b15.js"
 ])
