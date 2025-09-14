277deafd5d80b403612c169510f2ab85462c11f0
Arthur Brussee <arthur.brussee@gmail.com>
2025-09-14 17:48:11 +0100

    Update

--- _next/static/chunks/pages/_error.js
+++ _next/static/chunks/pages/_error.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_error", [
   "static/chunks/cedf9fb50f7951fc.js",
-  "static/chunks/a05c703d8e221053.js",
-  "static/chunks/fbaff89ac161fc40.js"
+  "static/chunks/7f4920b4ef105f50.js",
+  "static/chunks/ebc7818b405854b1.js"
 ])
