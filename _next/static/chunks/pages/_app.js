7788af7747608d50fc2631804c316ad0ae55e18b
Arthur Brussee <arthur.brussee@gmail.com>
2025-12-15 19:37:39 +0000

    Update

--- _next/static/chunks/pages/_app.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/36e7f54fe2e9aa85.js",
-  "static/chunks/a05c703d8e221053.js",
-  "static/chunks/c32f169d91dc2b15.js"
-])
--- _next/static/chunks/pages/_error.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/cedf9fb50f7951fc.js",
-  "static/chunks/a05c703d8e221053.js",
-  "static/chunks/fbaff89ac161fc40.js"
-])
